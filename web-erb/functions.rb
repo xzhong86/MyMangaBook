7ef81714166972a6c5ba4dde49326e851d75350f
BruceZ <xzhong86@163.com>
2020-02-04 13:02:13 +0800

    update functions, view.erb

--- web-erb/functions.rb
+++ web-erb/functions.rb
@@ -2,7 +2,8 @@
 require 'yaml'
 
 def load_books()
-  Dir.glob($env.root + '/book-*').sort.map do |dir|
+  return $env.books if $env.books
+  books = Dir.glob($env.root + '/book-*').sort.map do |dir|
     info = File.join(dir, 'info.yaml')
     if File.exist? info
       info = YAML.load(IO.read(info))
@@ -13,4 +14,26 @@ def load_books()
       nil
     end
   end.compact
+  $env.books = books
+  books
+end
+
+def get_next_book(cur)
+  load_books if not $env.books
+  idx = $env.books.index{ |b| b.dir == cur }
+  if idx and idx + 1 < cur.size
+    $env.books[idx+1].dir
+  else
+    nil
+  end
+end
+
+def get_prev_book(cur)
+  load_books if not $env.books
+  idx = $env.books.index{ |b| b.dir == cur }
+  if idx and idx - 1 >= 0
+    $env.books[idx-1].dir
+  else
+    nil
+  end
 end
